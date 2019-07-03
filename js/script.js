89820c18c4e24e91dfdcc75d7d278a560c34bdc6
Aleksandr Zhuravlev <zhuravlev.a.a@me.com>
2019-07-03 12:31:40 +0300

    меняю принцип скрытия модального окна

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
 var link = document.querySelector(".search-hotels-button");
 var popup = document.querySelector(".modal-booking");
-var close = popup.querySelector(".modal-show");
+var open = document.querySelector(".modal-hide");
 var checkIn = popup.querySelector(".check-in-value");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  if (document.querySelector(".modal-show")) {
-    popup.classList.remove("modal-show");
-  } else {
-    popup.classList.add("modal-show");
-    checkIn.focus();
-  }
+  popup.classList.add("modal-hide");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-      popup.classList.remove("modal-error");
+    if (popup) {
+      popup.classList.add("modal-hide");
     }
   }
 });
